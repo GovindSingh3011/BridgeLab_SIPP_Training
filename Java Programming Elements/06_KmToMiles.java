1c1001b169f7aea54cadf501556d4c45fd527dae
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-13 10:52:12 +0530

    [Govind Singh] Add . 06_KmToMiles

---
+++ Java Programming Elements/06_KmToMiles.java
@@ -0,0 +1,7 @@
+public class 06_KmToMiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km * 1.6;
+        System.out.printf("The distance "+km+" km in miles is "+miles);
+    }
+}
