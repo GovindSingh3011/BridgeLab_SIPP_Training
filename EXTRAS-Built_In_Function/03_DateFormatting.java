602594192db843226e3edb833632fbe00e52af40
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 11:01:58 +0530

    [Govind Singh] Add . 03_DateFormatting

---
+++ EXTRAS-Built_In_Function/03_DateFormatting.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class 03_DateFormatting {
+    public static void main(String[] args) {
+        DateTimeFormatter format01 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter format02 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter format03 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+
+        LocalDate today = LocalDate.now();
+
+        System.out.println("Format 1: " + today.format(format1));
+        System.out.println("Format 2: " + today.format(format2));
+        System.out.println("Format 3: " + today.format(format3));
+    }
+}
