0ca99541cca2743b58d233a6127ea632e4079f4e
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:10:56 +0530

    [Govind Singh] Fix . rename 01_DiscountCalculation.java to Q01_DiscountCalculation.java

--- Java Programming Elements/Q01_DiscountCalculation.java
+++ Java Programming Elements/Q01_DiscountCalculation.java
@@ -1,4 +1,4 @@
-public class 01_DiscountCalculation {
+public class Q01_DiscountCalculation {
     public static void main(String[] args) {
         int fee = 125000;
         int discountPercent = 10;
