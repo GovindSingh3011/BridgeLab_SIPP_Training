8ff33c932e8f3f5ec0a6eefa47f81a63929261ce
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:17:40 +0530

    [Govind Singh] Fix . rename 03_VotingEligibility.java to Q03_VotingEligibility.java

--- Arrays/Q03_VotingEligibility.java
+++ Arrays/Q03_VotingEligibility.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 03_VotingEligibility {
+public class Q03_VotingEligibility {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] ages = new int[10];
