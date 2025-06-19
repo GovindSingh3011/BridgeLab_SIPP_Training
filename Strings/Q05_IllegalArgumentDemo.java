e8581722fdb89994c2b722a850395f3d13faa10e
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 12:09:33 +0530

    [Govind Singh] Fix . rename 05_IllegalArgumentDemo.java to Q05_IllegalArgumentDemo.java

--- Strings/Q05_IllegalArgumentDemo.java
+++ Strings/Q05_IllegalArgumentDemo.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
 
-public class 05_IllegalArgumentDemo {
-    // Method to generate IllegalArgumentException
+public class Q05_IllegalArgumentDemo {
     public static void generateException(String text, int start, int end) {
-        // This will throw StringIndexOutOfBoundsException (which is a subclass of IndexOutOfBoundsException)
         System.out.println(text.substring(start, end));
     }
 
-    // Method to handle IllegalArgumentException
     public static void handleException(String text, int start, int end) {
         try {
             System.out.println(text.substring(start, end));
@@ -26,8 +23,6 @@ public class 05_IllegalArgumentDemo {
         int start = sc.nextInt();
         System.out.print("Enter end index: ");
         int end = sc.nextInt();
-        // Uncomment to see the exception
-        // generateException(input, end, start);
         System.out.println("Demonstrating IllegalArgumentException handling:");
         handleException(input, end, start);
     }
