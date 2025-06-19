919b3cd5ea65da99672ce42f261ed42dfe669cbe
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:27:08 +0530

    [Govind Singh] Fix . rename 39_AbundantNumber.java to Q39_AbundantNumber.java

--- Control Flows/Q39_AbundantNumber.java
+++ Control Flows/Q39_AbundantNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 39_AbundantNumber {
+public class Q39_AbundantNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt(), sum = 0;
