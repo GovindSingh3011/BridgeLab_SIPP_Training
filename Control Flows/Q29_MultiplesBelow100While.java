d01efccbedc2d76a7f4c8e2505d65268cd7de263
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:24:38 +0530

    [Govind Singh] Fix . rename 29_MultiplesBelow100While.java to Q29_MultiplesBelow100While.java

--- Control Flows/Q29_MultiplesBelow100While.java
+++ Control Flows/Q29_MultiplesBelow100While.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 29_MultiplesBelow100While {
+public class Q29_MultiplesBelow100While {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), counter = 99;
