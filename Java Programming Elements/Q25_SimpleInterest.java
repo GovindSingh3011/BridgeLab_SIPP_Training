7efdee93449c3c43729ecc508635382f6e7c01cd
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:11:01 +0530

    [Govind Singh] Fix . rename 25_SimpleInterest.java to Q25_SimpleInterest.java

--- Java Programming Elements/Q25_SimpleInterest.java
+++ Java Programming Elements/Q25_SimpleInterest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 25_SimpleInterest {
+public class Q25_SimpleInterest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter principal: ");
