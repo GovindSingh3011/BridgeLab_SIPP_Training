eade7b2361582e07e2ffd6fdb90189ea5d3bb072
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:24:36 +0530

    [Govind Singh] Fix . rename 20_FactorsWhile.java to Q20_FactorsWhile.java

--- Control Flows/Q20_FactorsWhile.java
+++ Control Flows/Q20_FactorsWhile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 20_FactorsWhile {
+public class Q20_FactorsWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), i = 1;
