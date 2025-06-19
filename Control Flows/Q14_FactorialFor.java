f5484dd538564de2f6c880b584dc5e6f58144ce4
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:24:35 +0530

    [Govind Singh] Fix . rename 14_FactorialFor.java to Q14_FactorialFor.java

--- Control Flows/Q14_FactorialFor.java
+++ Control Flows/Q14_FactorialFor.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 14_FactorialFor {
+public class Q14_FactorialFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt(), factorial = 1;
