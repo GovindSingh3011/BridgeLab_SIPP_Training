489fb46cbdb7b0d6f08ac0111046b2e6a79a56ae
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:17:44 +0530

    [Govind Singh] Fix . rename 18_DigitFrequency.java to Q18_DigitFrequency.java

--- Arrays/Q18_DigitFrequency.java
+++ Arrays/Q18_DigitFrequency.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 18_DigitFrequency {
+public class Q18_DigitFrequency {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
