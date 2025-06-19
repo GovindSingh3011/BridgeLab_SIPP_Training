52c8ec0f1836b846f8dac48440412a15c80a359c
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:24:35 +0530

    [Govind Singh] Fix . rename 12_SumUntilZeroBreak.java to Q12_SumUntilZeroBreak.java

--- Control Flows/Q12_SumUntilZeroBreak.java
+++ Control Flows/Q12_SumUntilZeroBreak.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 12_SumUntilZeroBreak {
+public class Q12_SumUntilZeroBreak {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double total = 0.0;
