910cef13d03affb2639c717672ffc4d4388ba63e
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:11:01 +0530

    [Govind Singh] Fix . rename 26_WeightConverter.java to Q26_WeightConverter.java

--- Java Programming Elements/Q26_WeightConverter.java
+++ Java Programming Elements/Q26_WeightConverter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 26_WeightConverter {
+public class Q26_WeightConverter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter weight in pounds: ");
