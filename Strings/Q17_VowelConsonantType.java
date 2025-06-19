cdcc2513932d1a3efab34135c3951f4a51a92086
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 12:24:11 +0530

    [Govind Singh] Fix . rename 17_VowelConsonantType.java to Q17_VowelConsonantType.java

--- Strings/Q17_VowelConsonantType.java
+++ Strings/Q17_VowelConsonantType.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class 17_VowelConsonantType {
-    // Method to check if character is vowel, consonant, or not a letter
+public class Q17_VowelConsonantType {
     public static String checkCharType(char c) {
         if (c >= 'A' && c <= 'Z') c = (char)(c + 32); // to lowercase
         if (c >= 'a' && c <= 'z') {
@@ -11,7 +10,6 @@ public class 17_VowelConsonantType {
         return "Not a Letter";
     }
 
-    // Method to get character and type in 2D array
     public static String[][] getCharTypes(String str) {
         String[][] arr = new String[str.length()][2];
         for (int i = 0; i < str.length(); i++) {
@@ -21,7 +19,6 @@ public class 17_VowelConsonantType {
         return arr;
     }
 
-    // Method to display 2D array in tabular format
     public static void displayTable(String[][] arr) {
         System.out.println("Char\tType");
         for (String[] row : arr) {
