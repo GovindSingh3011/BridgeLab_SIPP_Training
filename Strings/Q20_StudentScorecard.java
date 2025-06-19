d3dff136c78ea399a21a53f7c80eb677d85fb7e9
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 12:24:53 +0530

    [Govind Singh] Fix . rename 20_StudentScorecard.java to Q20_StudentScorecard.java

--- Strings/Q20_StudentScorecard.java
+++ Strings/Q20_StudentScorecard.java
@@ -1,22 +1,20 @@
 import java.util.Random;
 
-public class 20_StudentScorecard {
-    // Method to generate random 2-digit PCM scores for n students
+public class Q20_StudentScorecard {
     public static int[][] generateScores(int n) {
         int[][] scores = new int[n][3];
         Random rand = new Random();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 3; j++) {
-                scores[i][j] = 10 + rand.nextInt(91); // scores between 10 and 100
+                scores[i][j] = 10 + rand.nextInt(91);
             }
         }
         return scores;
     }
 
-    // Method to calculate total, average, and percentage for each student
     public static double[][] calculateStats(int[][] scores) {
         int n = scores.length;
-        double[][] stats = new double[n][3]; // total, average, percentage
+        double[][] stats = new double[n][3];
         for (int i = 0; i < n; i++) {
             int total = scores[i][0] + scores[i][1] + scores[i][2];
             double avg = total / 3.0;
@@ -28,7 +26,6 @@ public class 20_StudentScorecard {
         return stats;
     }
 
-    // Method to calculate grade based on percentage (as per image)
     public static String[] calculateGrades(double[][] stats) {
         int n = stats.length;
         String[] grades = new String[n];
@@ -44,7 +41,6 @@ public class 20_StudentScorecard {
         return grades;
     }
 
-    // Method to display the scorecard
     public static void displayScorecard(int[][] scores, double[][] stats, String[] grades) {
         System.out.println("Roll\tPhysics\tChemistry\tMaths\tTotal\tAverage\t%\tGrade");
         for (int i = 0; i < scores.length; i++) {
@@ -54,7 +50,7 @@ public class 20_StudentScorecard {
     }
 
     public static void main(String[] args) {
-        int n = 10; // number of students
+        int n = 10;
         int[][] scores = generateScores(n);
         double[][] stats = calculateStats(scores);
         String[] grades = calculateGrades(stats);
