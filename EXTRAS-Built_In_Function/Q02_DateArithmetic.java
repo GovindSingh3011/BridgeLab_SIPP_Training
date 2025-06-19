231be4041c9bf49ff36ae7c18ffcaa6164357d48
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-06-19 11:37:13 +0530

    [Govind Singh] Fix . rename 02_DateArithmetic.java to Q02_DateArithmetic.java

--- EXTRAS-Built_In_Function/Q02_DateArithmetic.java
+++ EXTRAS-Built_In_Function/Q02_DateArithmetic.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 
-public class 02_DateArithmetic {
+public class Q02_DateArithmetic {
     public static void main(String[] args) {
         LocalDate date = LocalDate.of(2025, 6, 19);
         LocalDate addDate = date.plusDays(7).plusMonths(1).plusYears(2);
