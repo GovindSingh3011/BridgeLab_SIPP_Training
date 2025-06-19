1ff64d3e31dfaf3aa2b79e99fc91e52062339748
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:17:42 +0530

    [Govind Singh] Fix . rename 10_Copy2DTo1D.java to Q10_Copy2DTo1D.java

--- Arrays/Q10_Copy2DTo1D.java
+++ Arrays/Q10_Copy2DTo1D.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 10_Copy2DTo1D {
+public class Q10_Copy2DTo1D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
