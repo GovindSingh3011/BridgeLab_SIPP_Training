d68b38e337e4c60efcc691f4223f3c26695a806c
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:17:09 +0530

    [Govind Singh] Fix . rename 01_FizzBuzz.java to Q01_FizzBuzz.java

--- Arrays/Q01_FizzBuzz.java
+++ Arrays/Q01_FizzBuzz.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 01_FizzBuzz {
+public class Q01_FizzBuzz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a positive number: ");
