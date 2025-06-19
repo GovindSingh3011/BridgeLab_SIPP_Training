880fbfca94bbea8d3ce501066fbb4e3d9747759f
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:11:00 +0530

    [Govind Singh] Fix . rename 19_SwapNumbers.java to Q19_SwapNumbers.java

--- Java Programming Elements/Q19_SwapNumbers.java
+++ Java Programming Elements/Q19_SwapNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 19_SwapNumbers {
+public class Q19_SwapNumbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter first number: ");
