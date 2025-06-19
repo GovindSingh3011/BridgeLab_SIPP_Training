32081001e4224fb16bee353b4b639abea8e30825
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:11:00 +0530

    [Govind Singh] Fix . rename 21_TravelDistance.java to Q21_TravelDistance.java

--- Java Programming Elements/Q21_TravelDistance.java
+++ Java Programming Elements/Q21_TravelDistance.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 21_TravelDistance {
+public class Q21_TravelDistance {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter name: ");
