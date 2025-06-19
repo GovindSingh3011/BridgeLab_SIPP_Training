a2d345b700e050f9da55eb1239f5f0775f5160c1
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-06-19 11:36:41 +0530

    [Govind Singh] Fix . rename 01_CurrentTimeZones.java to Q01_CurrentTimeZones.java

--- EXTRAS-Built_In_Function/Q01_CurrentTimeZones.java
+++ EXTRAS-Built_In_Function/Q01_CurrentTimeZones.java
@@ -1,6 +1,6 @@
 import java.time.*;
 
-public class 01_CurrentTimeZones {
+public class Q01_CurrentTimeZones {
     public static void main(String[] args) {
         ZonedDateTime gmt = ZonedDateTime.now(ZoneId.of("GMT"));
         ZonedDateTime ist = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
