849bac5d212a61e3a7066b427137c892cd244210
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 11:01:36 +0530

    [Govind Singh] Add . 02_DateArithmetic

---
+++ EXTRAS-Built_In_Function/02_DateArithmetic.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class 02_DateArithmetic {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2025, 6, 19);
+        LocalDate addDate = date.plusDays(7).plusMonths(1).plusYears(2);
+        LocalDate finalDate = addDate.minusWeeks(3);
+        System.out.println("Final Date: " + finalDate);
+    }
+}
