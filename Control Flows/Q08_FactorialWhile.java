4b47bb0cc1eb603f08d56d844ab94ad3e711f643
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:24:34 +0530

    [Govind Singh] Fix . rename 08_FactorialWhile.java to Q08_FactorialWhile.java

--- Control Flows/Q08_FactorialWhile.java
+++ Control Flows/Q08_FactorialWhile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 08_FactorialWhile {
+public class Q08_FactorialWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
