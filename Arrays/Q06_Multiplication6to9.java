4e23974e54f0891212783e26973b85719835cbc0
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:17:41 +0530

    [Govind Singh] Fix . rename 06_Multiplication6to9.java to Q06_Multiplication6to9.java

--- Arrays/Q06_Multiplication6to9.java
+++ Arrays/Q06_Multiplication6to9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 06_Multiplication6to9 {
+public class Q06_Multiplication6to9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
