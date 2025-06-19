9536d8aaee061924ad07c652208758c24f05138f
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 12:38:25 +0530

    [Govind Singh] Fix . rename 05_WindChill.java to Q05_WindChill.java

--- Methods/Q05_WindChill.java
+++ Methods/Q05_WindChill.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 05_WindChill {
+public class Q05_WindChill {
     public static double calculateWindChill(double temperature, double windSpeed) {
         return 35.74 + 0.6215 * temperature + (0.4275 * temperature - 35.75) * windSpeed;
     }
