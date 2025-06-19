165affb19ebf9f252f2a45cbb4816e6128afe10e
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:10:59 +0530

    [Govind Singh] Fix . rename 16_PurchasePrice.java to Q16_PurchasePrice.java

--- Java Programming Elements/Q16_PurchasePrice.java
+++ Java Programming Elements/Q16_PurchasePrice.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 16_PurchasePrice {
+public class Q16_PurchasePrice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter unit price: ");
