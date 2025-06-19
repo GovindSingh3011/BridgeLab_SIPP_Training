a9b2abf2e58c5911962f93c171be0bd180f196e1
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:24:37 +0530

    [Govind Singh] Fix . rename 23_FizzBuzzFor.java to Q23_FizzBuzzFor.java

--- Control Flows/Q23_FizzBuzzFor.java
+++ Control Flows/Q23_FizzBuzzFor.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 23_FizzBuzzFor {
+public class Q23_FizzBuzzFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
