f159c86ad8db699779c04120da32784068e50598
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:27:06 +0530

    [Govind Singh] Fix . rename 31_LeapYearIfElse.java to Q31_LeapYearIfElse.java

--- Control Flows/Q31_LeapYearIfElse.java
+++ Control Flows/Q31_LeapYearIfElse.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class 31_LeapYearIfElse {
+public class Q31_LeapYearIfElse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
