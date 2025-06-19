dd5a66d81805eb03150e659adf9ab190a99b89cc
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:10:59 +0530

    [Govind Singh] Fix . rename 15_DoubleOperation.java to Q15_DoubleOperation.java

--- Java Programming Elements/Q15_DoubleOperation.java
+++ Java Programming Elements/Q15_DoubleOperation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 15_DoubleOperation {
+public class Q15_DoubleOperation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a: ");
