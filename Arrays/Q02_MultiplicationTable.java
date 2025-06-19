807b002b3760fc666b2604236be441c09a2f5790
GovindSingh3011 <govindrawat3011@gmail.com>
2025-06-19 13:17:40 +0530

    [Govind Singh] Fix . rename 02_MultiplicationTable.java to Q02_MultiplicationTable.java

--- Arrays/Q02_MultiplicationTable.java
+++ Arrays/Q02_MultiplicationTable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 02_MultiplicationTable {
+public class Q02_MultiplicationTable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
